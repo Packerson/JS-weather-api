24ea9a71085995688f211ca6c69f0ce017c32d78
Pawel <you@example.com>
2023-01-28 19:05:02 +0100

    add_city problem with cloneNode

---
+++ build/0.e0686972592132de800b.hot-update.js
@@ -0,0 +1,19 @@
+self["webpackHotUpdatees6_async_await"](0,{
+
+/***/ 23:
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/pawel/workspace/kurs_java_script/JS-weather-api/js/app.js: Missing catch or finally clause. (14:8)\n\n\u001b[0m \u001b[90m 12 |\u001b[39m     (\u001b[32m'click'\u001b[39m\u001b[33m,\u001b[39m (ev) \u001b[33m=>\u001b[39m {\u001b[0m\n\u001b[0m \u001b[90m 13 |\u001b[39m         ev\u001b[33m.\u001b[39mpreventDefault()\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 14 |\u001b[39m         \u001b[36mtry\u001b[39m {\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 15 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 16 |\u001b[39m     }}\u001b[0m\n\u001b[0m \u001b[90m 17 |\u001b[39m )\u001b[0m\n    at instantiate (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:64:32)\n    at constructor (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:361:12)\n    at Parser.raise (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:3251:19)\n    at Parser.parseTryStatement (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:13009:12)\n    at Parser.parseStatementContent (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:12580:21)\n    at Parser.parseStatementLike (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:12535:17)\n    at Parser.parseStatementListItem (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:12522:17)\n    at Parser.parseBlockOrModuleBlockBody (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:13106:61)\n    at Parser.parseBlockBody (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:13099:10)\n    at Parser.parseBlock (/home/pawel/workspace/kurs_java_script/JS-weather-api/node_modules/@babel/parser/lib/index.js:13087:10)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("79831778b4817854a568")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5lMDY4Njk3MjU5MjEzMmRlODAwYi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2VzNi1hc3luYy1hd2FpdC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiNzk4MzE3NzhiNDgxNzg1NGE1NjhcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
